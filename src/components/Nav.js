149873c05d5c63eb7d6857d775a3abb8fa97316b
Priyanka Wankhade <58451919+pgwankhade@users.noreply.github.com>
2020-10-28 22:20:38 +0530

    Update Nav.js
    changed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo1 from "./logo.jpg"
 
 
+
 class Nav extends React.Component {
   constructor(props) {
     super(props);
@@ -36,10 +37,10 @@ class Nav extends React.Component {
       <div>
       <nav className="dt w-100 bg-lightest-blue border-box pa3 ph5-ns">
       <p className="dtc v-mid white link dim" >
-      <img src={Logo1} className="dib w4 h3" alt="Site Name"/>
+      <img src={Logo1} className="dib w3 h2 mr3" alt="Site Name"/>
       </p>
-     <div className=" nav2 f6 fw7 ttu tracked">
-     <p className="link dim dark-blue f6 dib mr2 ml7" >Daily Tasks</p>
+     <div className="nav2 f6 fw7 ttu tracked">
+     <p className="link dim dark-blue f6 dib ml0" >Daily Tasks</p>
      <p onClick={this.onlogout} className=" tr pointer dark-blue f6 mt0 " >Log Out</p>
       </div>
       {this.state.loading?<h1 className=" tr dark-blue f6 mt0 ">loging out...</h1>:""}
